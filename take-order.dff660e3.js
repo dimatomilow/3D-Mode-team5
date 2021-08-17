7e0dc6914d27bf474e9bb7d7854df31ab5617731
dimatomilow <84626287+dimatomilow@users.noreply.github.com>
2021-08-17 13:28:05 +0000

    Deploying to gh-pages from @ dimatomilow/3D-Mode-team5@a2381b03bbae3b04bb2b0aff1fdbc9271d6d1891 🚀

--- take-order.dff660e3.js
+++ take-order.dff660e3.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../fonts/CeraPro-Regular.woff":[["CeraPro-Regular.041c7bd2.woff","s2hl"],"s2hl"],"./../fonts/CaviarDreams.woff":[["CaviarDreams.f484dfa7.woff","lSNS"],"lSNS"]}],"yDji":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["yDji"], null)
-//# sourceMappingURL=/3D-Mode-team5/take-order.e2c7123a.js.map
+//# sourceMappingURL=/3D-Mode-team5/take-order.dff660e3.js.map
